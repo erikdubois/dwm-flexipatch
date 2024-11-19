25c64b3f55a1bdff1ce2e4796c4785c4d872a671
Erik Dubois <erik.dubois@gmail.com>
2024-11-19 19:30:39 +0100

    arandr

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output None-1-1 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --off
 
 function run {
  if ! pgrep $1 ;
