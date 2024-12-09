c01de2bd7609f509d9dbe546de14a7ebb641203b
arcolinuxz <arcolinuxinfo@gmail.com>
2024-12-09 07:38:35 +0100

    arco folder

--- autostart.sh
+++ autostart.sh
@@ -46,7 +46,7 @@ sxhkd -c ~/.config/flexi/sxhkdrc &
 #run "conky -c $HOME/.config/arco-dwm/system-overview"
 #you can set wallpapers in themes as well
 feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arco/arco-wallpaper.jpg &
 #wallpaper for other Arch based systems
 #feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 feh --bg-fill /usr/share/backgrounds/flexi.png &
